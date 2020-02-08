3ce8a4abb4c38483d63666b08171c0084411e3ee
Sarah <srucci1103@gmail.com>
2020-02-07 22:42:00 -0500

    Finished

--- dev/js/jbox.js
+++ dev/js/jbox.js
@@ -1,28 +1,32 @@
-new jBox('Tooltip', {
-  attach: '#tooltip',
-  theme: 'TooltipDark',
-  trigger: 'mouseenter',
-  responsiveWidth: 'boolean',
-  height: 50
 
+$('#tooltip').jBox('Tooltip', {
+    theme: 'TooltipDark',
+    trigger: 'mouseenter',
+    responsiveWidth: 'boolean',
+    height: 50
+ });
 
+ $('#downloadbutton').jBox('Mouse', {
+     trigger: 'mouseenter',
+     content: 'I will take you to GitHub!'
+  });
+
+ $('#modal').jBox('Modal', {
+     color: 'black',
+     title: 'This is a window.',
+     content: '<i>This is a sentence!</i>'
 });
 
-new jBox('Modal', {
-  width: 30,
-  height: 10,
-  attach: '#modal',
-  title: 'My Modal Window',
-  content: '<i>Hello there!</i>'
-});
 
-new jBox('Notice', {
-attach: '#notice',
-  content: 'Hurray! A notice!',
-  color: 'blue'
+function demoNotice() {
+$('#notice').jBox('Notice', {
+      trigger: 'click',
+      content: 'Here is a notice!',
+      color: 'black'
 });
+}
 
-new jBox('Confirm', {
-  confirmButton: 'Do it!',
-  cancelButton: 'Nope'
+$('#confirm').jBox('Confirm', {
+    confirmButton: 'Yes I did!',
+    cancelButton: 'Please close this.'
 });
