78b3ac21af8117b0267b93dea7758556f4f39b14
Sarah <srucci1103@gmail.com>
2020-02-10 20:31:06 -0500

    Cleanup

--- dev/js/jbox.js
+++ dev/js/jbox.js
@@ -2,14 +2,8 @@
 $('#tooltip').jBox('Tooltip', {
     theme: 'TooltipDark',
     trigger: 'mouseenter',
-    responsiveWidth: 'boolean',
     height: 50
  });
-
- $('#downloadbutton').jBox('Mouse', {
-     trigger: 'mouseenter',
-     content: 'I will take you to GitHub!'
-  });
 
  $('#modal').jBox('Modal', {
      color: 'black',
@@ -17,16 +11,19 @@ $('#tooltip').jBox('Tooltip', {
      content: '<i>This is a sentence!</i>'
 });
 
-
 function demoNotice() {
 $('#notice').jBox('Notice', {
       trigger: 'click',
       content: 'Here is a notice!',
       color: 'black'
-});
-}
+});}
 
 $('#confirm').jBox('Confirm', {
     confirmButton: 'Yes I did!',
     cancelButton: 'Please close this.'
 });
+
+$('#downloadbutton').jBox('Mouse', {
+    trigger: 'mouseenter',
+    content: 'I will take you to GitHub!'
+ });
